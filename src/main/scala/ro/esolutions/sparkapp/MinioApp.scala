ba1c80e242d16ed4bfd204411d66c6975ac7bc9f
Lucian Neghina <lucian.neghina@esolutions.ro>
2021-04-27 21:48:59 +0300

    read from minio

---
+++ src/main/scala/ro/esolutions/sparkapp/MinioApp.scala
@@ -0,0 +1,26 @@
+package ro.esolutions.sparkapp
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+
+object MinioApp {
+  def main(args: Array[String]) {
+
+    val jobName = this.getClass.getCanonicalName
+    val conf = new SparkConf().setAppName(jobName)
+      .set("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")
+      .set("spark.hadoop.fs.s3a.access.key", "minioadmin")
+      .set("spark.hadoop.fs.s3a.secret.key", "minioadmin")
+      .set("spark.hadoop.fs.s3a.path.style.access", "true")
+      .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark.read.csv("s3a://spark/input/orders.csv")
+
+    println(df.show(10))
+
+    df.printSchema()
+
+  }
+}
