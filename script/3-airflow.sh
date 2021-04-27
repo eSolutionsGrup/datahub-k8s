fe6343657b1c7e318648d00d51a4f4994b82ba6f
Lucian Neghina <lucian.neghina@esolutions.ro>
2021-04-27 17:52:24 +0300

    airflow

--- script/3-airflow.sh
+++ script/3-airflow.sh
@@ -19,3 +19,52 @@ helm install \
   --namespace $NAMESPACE \
   --version $CHART_VERSION \
   -f $VALUES_FILE
+
+
+######## TBD ##########
+
+#[2021-04-27 08:21:57,522] {taskinstance.py:1455} ERROR - Exception when calling -> create_custom_object: (403)
+#Reason: Forbidden
+#HTTP response headers: HTTPHeaderDict({'Cache-Control': 'no-cache, private', 'Content-Type': 'application/json', 'X-Content-Type-Options': 'nosniff', 'Date': 'Tue, 27 Apr 2021 08:21:57 GMT', 'Content-Length': '398'})
+#HTTP response body: {"kind":"Status","apiVersion":"v1","metadata":{},"status":"Failure","message":"sparkapplications.sparkoperator.k8s.io is forbidden: User \"system:serviceaccount:airflow:airflow\" cannot create resource \"sparkapplications\" in API group \"sparkoperator.k8s.io\" in the namespace \"spark-apps\"","reason":"Forbidden","details":{"group":"sparkoperator.k8s.io","kind":"sparkapplications"},"code":403}
+
+
+
+# kubectl create clusterrolebinding cluster-admin \
+#  --clusterrole=cluster-admin \
+#  --serviceaccount=airflow:airflow 
+
+
+
+
+# kubectl create serviceaccount airflow --namespace=spark-apps
+
+# kubectl create clusterrolebinding airflow-spark-role \
+#  --clusterrole=edit \
+#  --serviceaccount=airflow:airflow \
+#  --namespace=spark-apps
+
+
+# kubectl create clusterrolebinding airflow-admin \
+#  --clusterrole=cluster-admin \
+#  --serviceaccount=airflow:airflow \
+#  --namespace=spark-apps 
+
+
+
+
+
+# kubectl create serviceaccount airflow --namespace=spark-operator
+# kubectl create clusterrolebinding airflow-operator-role \
+#  --clusterrole=edit \
+#  --serviceaccount=spark-operator:airflow \
+#  --namespace=spark-operator
+
+
+
+
+# kubectl create clusterrolebinding airflow-admin \
+#  --clusterrole=cluster-admin \
+#  --serviceaccount=airflow:airflow \
+#  --namespace=spark-apps 
+
--- script/5-running.sh
+++ script/5-running.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+kubectl delete -f orders.yaml
+
 kubectl apply -f orders.yaml
