f78855a8e25b88aa368aa4cbdab0b7f45afba319
Lucian Neghina <lucian.neghina@esolutions.ro>
2022-01-05 16:46:43 +0200

    read & write to minio

--- src/main/scala/ro/esolutions/sparkapp/MinioApp.scala
+++ src/main/scala/ro/esolutions/sparkapp/MinioApp.scala
@@ -16,6 +16,10 @@ import org.apache.spark.sql._
 - spark.hadoop.fs.s3a.impl = org.apache.hadoop.spark.hadoop.fs.s3a.S3AFileSystem
 - spark.hadoop.fs.defaultFS = s3a://deltalake
  *
+ *
+ * Disable SSL certificate verification (local running)
+ * -Dcom.amazonaws.sdk.disableCertChecking=true
+ *
  */
 
 object MinioApp {
@@ -32,35 +36,34 @@ object MinioApp {
       .set("spark.hadoop.fs.s3a.fast.upload", "true")
       .set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
       .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-      .set("spark.hadoop.fs.defaultFS", "s3a://test")
+      .set("spark.hadoop.fs.defaultFS", "s3a://demo")
 
     val spark = SparkSession.builder()
-      .master("local")
+      .master("local[*]")
       .config(conf)
       .getOrCreate()
-//    spark.sparkContext.setLogLevel("ERROR")
+    spark.sparkContext.setLogLevel("ERROR")
 
 
     println("-" * 30)
 
+//    spark.range(1,10)
+//      .write
+//      .mode("overwrite")
+//      .format("csv")
+//      .save("/dummy")
 
-    spark.range(1,10)
-      .write
+    println("-" * 30)
+
+    println(" reading from minio and write to delta lake over minio")
+    val df = spark.read.csv("/orders.csv")
+      .withColumnRenamed("_c0", "id")
+      .withColumnRenamed("_c1", "date")
+
+    df.write
       .mode("overwrite")
-      .format("csv")
-      .save("/demo/main/dummy")
-
-//    println(spark.sessionState.conf.getAllConfs)
-
-//    spark.sessionState.conf.getAllConfs.foreach(case (k, v) => {println(s"$k -> $v")})
-
-    println("-" * 30)
-
-//    val df = spark.read.csv("/input/orders.csv")
-//
-//    println(df.show(10))
-//
-//    df.write.mode(SaveMode.Overwrite).json("/output/")
+      .format("delta")
+      .save("/delta")
 
   }
 }
